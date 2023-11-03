cab1c858cc5626a290bd621c092dcd94f112fda1
Luofan Chen <clfbbn@gmail.com>
2023-11-03 13:08:31 +0800

    framework: Add dummy libraries
    So that one can install some sony apps successfully.
    
    Change-Id: Idd71e351585c4dec15d3b0190110381a04b0d432

---
+++ framework/sony_device/src/com/sony/device/Dummy.java
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.sony.device;
+
+public final class Dummy {
+    private Dummy() {
+    }
+}
