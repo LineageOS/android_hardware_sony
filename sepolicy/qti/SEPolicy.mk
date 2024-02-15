adbb9a68888987115f21ca3899c6c83503bd38d6
Luofan Chen <clfbbn@gmail.com>
2024-02-15 22:25:17 +0800

    sepolicy: Populate initial common SEPolicy
    Change-Id: I8564678af2b6759c2a27d9daf105f36f2c37bf1a

---
+++ sepolicy/qti/SEPolicy.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+SEPOLICY_PLATFORM := $(subst device/qcom/sepolicy_vndr/,,$(SEPOLICY_PATH))
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    hardware/sony/sepolicy/qti/vendor \
+    hardware/sony/sepolicy/qti/vendor/$(SEPOLICY_PLATFORM)
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    hardware/sony/sepolicy/qti/private \
+    hardware/sony/sepolicy/qti/private/$(SEPOLICY_PLATFORM)
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    hardware/sony/sepolicy/qti/public \
+    hardware/sony/sepolicy/qti/public/$(SEPOLICY_PLATFORM)
+
+ifneq ($(SEPOLICY_PLATFORM), legacy-um)
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    hardware/sony/sepolicy/qti/vendor/common-um
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    hardware/sony/sepolicy/qti/private/common-um
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    hardware/sony/sepolicy/qti/public/common-um
+endif
+
+ifneq (,$(filter lahaina, $(TARGET_BOARD_PLATFORM)))
+    BOARD_VENDOR_SEPOLICY_DIRS += \
+        hardware/sony/sepolicy/qti/vendor/lahaina
+endif
