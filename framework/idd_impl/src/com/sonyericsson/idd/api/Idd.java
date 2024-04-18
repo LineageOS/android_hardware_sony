5aadc574138d0e29ff18f898613002c307c7dbe2
Luofan Chen <clfbbn@gmail.com>
2024-04-18 23:15:51 +0800

    framework: idd_impl: Add dummy addAppDataJSON method
    Change-Id: Id3ff7d6d9d3b28d00582dafc0e63d77a198c971c

--- framework/idd_impl/src/com/sonyericsson/idd/api/Idd.java
+++ framework/idd_impl/src/com/sonyericsson/idd/api/Idd.java
@@ -1,11 +1,17 @@
 /*
- * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-FileCopyrightText: 2023-2024 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
 package com.sonyericsson.idd.api;
 
+import org.json.JSONObject;
+
 public final class Idd {
     public static void addEvent(String pkg, String name, byte[] payload) {
+    }
+
+    public static void addAppDataJSON(String packageName, String versionName, int versionCode,
+            JSONObject payload) {
     }
 }
