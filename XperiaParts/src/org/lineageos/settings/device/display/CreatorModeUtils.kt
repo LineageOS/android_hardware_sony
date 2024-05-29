78e1aa84cc0cea05e97691151d1557b7b37b380b
Luofan Chen <clfbbn@gmail.com>
2024-05-29 11:20:39 +0800

    XperiaParts: Don't setup display HAL on boot if setting is disabled
    Signed-off-by: Luofan Chen <clfbbn@gmail.com>
    Change-Id: I1d3566049e9c8581a600dfe745fac257b2dfbcd6

--- XperiaParts/src/org/lineageos/settings/device/display/CreatorModeUtils.kt
+++ XperiaParts/src/org/lineageos/settings/device/display/CreatorModeUtils.kt
@@ -20,8 +20,11 @@ class CreatorModeUtils(private val context: Context) : IDisplayCallback.Stub() {
     private val colorDisplayManager: ColorDisplayManager =
             context.getSystemService(ColorDisplayManager::class.java)
                     ?: throw Exception("Display manager is NULL")
-    private val semcDisplayService: IDisplay =
-            IDisplay.getService() ?: throw Exception("SEMC Display HIDL not found")
+    private val semcDisplayService: IDisplay by lazy {
+        val service = IDisplay.getService() ?: throw Exception("SEMC Display HIDL not found")
+        service.setup()
+        service
+    }
     private val liveDisplayManager: LiveDisplayManager = LiveDisplayManager.getInstance(context)
 
     val isEnabled: Boolean
@@ -40,7 +43,11 @@ class CreatorModeUtils(private val context: Context) : IDisplayCallback.Stub() {
 
         // Register itself as callback for HIDL
         semcDisplayService.registerCallback(this)
-        semcDisplayService.setup()
+
+        // Don't apply anything if the setting is disabled
+        if (isEnabled) {
+            setMode(true)
+        }
     }
 
     override fun onWhiteBalanceMatrixChanged(matrix: PccMatrix) {
