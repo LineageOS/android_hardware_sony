c54c50b125f49f16fe2dc1153227d72cd451a97d
Luofan Chen <clfbbn@gmail.com>
2024-10-02 04:06:12 +0000

    fix
    Change-Id: I6c8713f462d8f425c274811c490bdb022d919f55

--- Euicc/src/org/lineageos/euicc/EuiccDisabler.kt
+++ Euicc/src/org/lineageos/euicc/EuiccDisabler.kt
@@ -27,13 +27,13 @@ object EuiccDisabler {
 
     private fun isInstalled(pm: PackageManager, pkgName: String) = runCatching {
         val info = pm.getPackageInfo(pkgName, PackageInfoFlags.of(0))
-        info.applicationInfo.flags and ApplicationInfo.FLAG_INSTALLED != 0
+        info.applicationInfo!!.flags and ApplicationInfo.FLAG_INSTALLED != 0
     }.getOrDefault(false)
 
     private fun isInstalledAndEnabled(pm: PackageManager, pkgName: String) = runCatching {
         val info = pm.getPackageInfo(pkgName, PackageInfoFlags.of(0))
-        Log.d(TAG, "package $pkgName installed, enabled = ${info.applicationInfo.enabled}")
-        info.applicationInfo.enabled
+        Log.d(TAG, "package $pkgName installed, enabled = ${info.applicationInfo!!.enabled}")
+        info.applicationInfo!!.enabled
     }.getOrDefault(false)
 
     fun enableOrDisableEuicc(context: Context) {
