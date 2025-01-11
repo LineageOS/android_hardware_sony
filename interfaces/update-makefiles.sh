7c38c3fb02147a24f8a91ba4f8eb47b3d0a7cb7d
LuK1337 <priv.luk@gmail.com>
2025-01-11 19:28:50 +0100

    interfaces: Add vendor.egistec.hardware.fingerprint@4.0
    * pdx225 -> IBiometricsFingerprintRbs
    * pdx235 -> IBiometricsFingerprintBix
    
    Change-Id: Ie2e53e98f36a2d6d219f9ece0c77a08b5c9e74a6

--- interfaces/update-makefiles.sh
+++ interfaces/update-makefiles.sh
@@ -3,4 +3,7 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
+  "vendor.egistec:hardware/sony/interfaces/egistec"
+
+do_makefiles_update \
   "vendor.semc:hardware/sony/interfaces/semc"
