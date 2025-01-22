2fdbde1ef23ee6c3512cc7e1079cd35799462df9
Alcatraz323 <alcatraz32323@gmail.com>
2025-01-22 22:16:48 +0800

    XperiaParts: Implement MediaVibration settings
    This implementation serves a reference to the mediavibration
    implementation on stock, and only works with stock audio HAL.
    
    The prebuilt audio HAL contains the implementation of the media
    vibration. So enable this setting if we are using the prebuilt audio HAL
    on that platform.
    
    More details on the media vibration implementation in audio HAL:
    
    - On kona platform: On initialization, it reads the media preset file in
      /vendor/etc/media_vib/media_vib_params_rev3. When enabled, the media
      vibration works by applying the "media-vibrator" audio route and sets
      the corresponding mixer values as specified in the mixer_paths.xml.
      When the volume changes, it will also write some values to the mixers
      as specified in the media_vib_params_rev3 file based on the music
      volume and the params in the file
    
    - On kalama platform: The "A2H Tuning" mixer seems to be used to set strength
      levels of the vibration, the "A2H Mute Switch" mixer seems to be used
      to enable mediavib, the "A2H Volume Level" should be changed when
      audio volume changed, and the "A2H delay" mixer is used to set the
      audio delay when playing through a2dp bluetooth headset.
    
    Change-Id: I6b929ae1a779c3bc39f9efa88c78a6dc4f09de80

--- XperiaParts/src/org/lineageos/settings/device/BootCompletedReceiver.kt
+++ XperiaParts/src/org/lineageos/settings/device/BootCompletedReceiver.kt
@@ -9,12 +9,23 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+
 import org.lineageos.settings.device.display.CreatorModeUtils
+import org.lineageos.settings.device.mediavib.MediaVibration
+import org.lineageos.settings.device.volsync.VolumeListenerService
 
 class BootCompletedReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         Log.d(TAG, "Starting")
         CreatorModeUtils(context).initialize()
+        if (MediaVibration.isMediaVibAvailable(context)) {
+            MediaVibration.enableActivity(context)
+            MediaVibration.onBoot(context)
+        } else {
+            // Remove this activity if the feature is not available
+            MediaVibration.disableActivity(context)
+            Log.i(TAG, "Media vibration is not available, removing the activity")
+        }
     }
 
     companion object {
---
+++ XperiaParts/src/org/lineageos/settings/device/mediavib/MediaVibration.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2025 Alcatraz323 <alcatraz32323@gmail.com>
+ * Copyright (C) 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.lineageos.settings.device.mediavib
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.pm.PackageManager
+import android.media.AudioManager
+import android.provider.Settings
+
+object MediaVibration {
+    fun switchMediaVibration(context: Context, enable: Boolean) {
+        val am = context.getSystemService(
+            AudioManager::class.java
+        )
+        val paramStr = if (enable) "1" else "0"
+        am!!.setParameters("somc.media_vibration=$paramStr")
+        Settings.Secure.putInt(
+            context.contentResolver,
+            MediaVibrationSettingsFragment.PREF_MEDIA_VIB_ENABLE,
+            if (enable) 1 else 0
+        )
+    }
+
+    fun isMediaVibAvailable(context: Context): Boolean {
+        val am = context.getSystemService(
+            AudioManager::class.java
+        )
+        val ret = am!!.getParameters("somc.media_vibration") != ""
+        return ret
+    }
+
+    fun disableActivity(context: Context) {
+        val pm = context.packageManager
+        pm.setComponentEnabledSetting(
+            ComponentName(context, MediaVibrationActivity::class.java),
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+            PackageManager.DONT_KILL_APP
+        )
+    }
+
+    fun enableActivity(context: Context) {
+        val pm = context.packageManager
+        pm.setComponentEnabledSetting(
+            ComponentName(context, MediaVibrationActivity::class.java),
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+            PackageManager.DONT_KILL_APP
+        )
+    }
+
+    fun setMediaVibrationLevel(context: Context, level: Int) {
+        val am = context.getSystemService(
+            AudioManager::class.java
+        )
+        am!!.setParameters("somc.media_vibration_vol_idx=$level")
+        Settings.Secure.putInt(
+            context.contentResolver,
+            MediaVibrationSettingsFragment.PREF_MEDIA_VIB_LEVEL,
+            level
+        )
+    }
+
+    fun getMediaVibrationState(context: Context): Boolean {
+        val state =
+            Settings.Secure.getInt(
+                context.contentResolver,
+                MediaVibrationSettingsFragment.PREF_MEDIA_VIB_ENABLE,
+                0
+            ) == 1
+        return state
+    }
+
+    fun getMediaVibrationLevel(context: Context): Int {
+        val level =
+            Settings.Secure.getInt(
+                context.contentResolver,
+                MediaVibrationSettingsFragment.PREF_MEDIA_VIB_LEVEL,
+                0
+            )
+        return level
+    }
+
+    fun onBoot(context: Context) {
+        switchMediaVibration(context, getMediaVibrationState(context))
+        setMediaVibrationLevel(context, getMediaVibrationLevel(context))
+    }
+}
