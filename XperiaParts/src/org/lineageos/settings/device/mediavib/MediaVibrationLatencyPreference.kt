78a52affeec56d60ad96d05156aa4c257e666781
Luofan Chen <clfbbn@gmail.com>
2025-02-02 17:06:03 +0800

    XperiaParts: Implement latency control in media vibration
    Change-Id: I85962e53b26dc2cb263432d9e07a63d91543d3b4

---
+++ XperiaParts/src/org/lineageos/settings/device/mediavib/MediaVibrationLatencyPreference.kt
@@ -0,0 +1,45 @@
+package org.lineageos.settings.device.mediavib
+
+import android.content.Context
+import android.widget.SeekBar
+import androidx.preference.Preference
+import android.widget.TextView
+import androidx.preference.PreferenceViewHolder
+
+import org.lineageos.settings.device.R
+
+class MediaVibrationLatencyPreference(context: Context) : Preference(context), SeekBar.OnSeekBarChangeListener {
+    private lateinit var mLatencyValue : TextView
+    private lateinit var mLatencyBar: SeekBar
+
+    init {
+        setLayoutResource(R.layout.preference_media_vibration_latency)
+    }
+
+    override fun onBindViewHolder(holder: PreferenceViewHolder) {
+        super.onBindViewHolder(holder)
+
+        mLatencyValue = holder.findViewById(R.id.latency_value)!! as TextView
+        mLatencyBar = holder.findViewById(R.id.latency_seekbar)!! as SeekBar
+        mLatencyBar.setOnSeekBarChangeListener(this)
+
+        val latency = MediaVibration.getMediaVibrationLatency(context)
+        mLatencyBar.progress = latency
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        mLatencyValue.text = String.format("utf-8", "%d", progress)
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+        MediaVibration.setMediaVibrationLatency(context, seekBar!!.progress)
+    }
+
+    companion object {
+        const val PREF_MEDIA_VIB_LATENCY: String = "mediavib_latency"
+    }
+
+}
