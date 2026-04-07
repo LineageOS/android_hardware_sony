dbfea6fd6b696abbb50095c6ea1d647e16b87f36
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2026-04-07 16:07:28 +0200

    vibrator: Add device-specific configuration parameters
    - use_compound_double_click_effect - seen TRUE on SM8350, where
      there may be not a hardware waveform for Effect::DOUBLE_CLICK
    
    - use_effect_duration_polling - seen TRUE on SM8450+, where
      pollVibeState accepts a timeout parameter, which has to be
      handled with a mutex.
    
    Change-Id: I10565306f612aaebef765a4f604fa0403f99c026

--- aidl/vibrator/cs40l25/Hardware.h
+++ aidl/vibrator/cs40l25/Hardware.h
@@ -72,9 +72,15 @@ class HwApi : public Vibrator::HwApi, private HwApiBase {
     bool setGpioFallScale(uint32_t value) override { return set(value, &mGpioFallScale); }
     bool setGpioRiseIndex(uint32_t value) override { return set(value, &mGpioRiseIndex); }
     bool setGpioRiseScale(uint32_t value) override { return set(value, &mGpioRiseScale); }
+
+#ifdef USE_EFFECT_DURATION_POLLING
     bool pollVibeState(uint32_t value, int32_t timeoutMs) override {
         return poll(value, &mVibeState, timeoutMs);
     }
+#else
+    bool pollVibeState(bool value) override { return poll(value, &mVibeState); }
+#endif
+
     bool setClabEnable(bool value) override { return set(value, &mClabEnable); }
     bool getAvailablePwleSegments(uint32_t* value) override {
         return get(value, &mAvailablePwleSegments);
